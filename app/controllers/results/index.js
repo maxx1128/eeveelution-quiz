490b307ce62d05cd2befc7cd821079e1c90248f7
Max Antonucci <maxx1128@gmail.com>
2019-04-01 18:18:19 -0400

    Hide winning result unless finished quiz

--- app/controllers/results/index.js
+++ app/controllers/results/index.js
@@ -1,10 +1,12 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
-import { reads } from '@ember/object/computed';
+import { reads, and } from '@ember/object/computed';
 
 export default Controller.extend({
   quiz: service(),
 
   all_pokemon: reads('quiz.answers_data'),
-  winner: reads('quiz.winner')
+  winner: reads('quiz.winner'),
+  completed: reads('quiz.completed'),
+  showWinner: and('winner', 'completed')
 });
