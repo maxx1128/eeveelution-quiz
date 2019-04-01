a8a9626a38c9472626f945b47f1bb865bff3a3c1
Max Antonucci <maxx1128@gmail.com>
2019-04-01 18:00:49 -0400

    Stylize pokedex entries

--- app/components/poke-entries.js
+++ app/components/poke-entries.js
@@ -2,12 +2,27 @@ import Component from '@ember/component';
 import { computed } from '@ember/object';
 import { filter } from '@ember/object/computed';
 
+function getUnique(arr, comp) {
+  const unique = arr
+       .map(e => e[comp])
+
+     // store the keys of the unique objects
+    .map((e, i, final) => final.indexOf(e) === i && i)
+
+    // eliminate the dead keys & store unique objects
+    .filter(e => arr[e]).map(e => arr[e]);
+
+   return unique;
+}
+
 export default Component.extend({
   englishEntries: filter('rawEntries', function(entry) {
     return entry.language.name == 'en';
   }),
 
-  reversedEntries: computed('englishEntries', function() {
-    return this.get('englishEntries').reverse();
+  filteredEntries: computed('englishEntries', function() {
+    const reversedArray = this.get('englishEntries').reverse();
+
+    return getUnique(reversedArray, 'flavor_text');
   })
 });
